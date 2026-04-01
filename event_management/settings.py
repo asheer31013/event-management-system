b4a3ff22099712922f4bf647fa0fee8f1bf9e9a2
M Asheer Ahamed <m.asheer31013@gmail.com>
2026-04-01 06:40:45 +0530

    Ready for deployment

--- event_management/settings.py
+++ event_management/settings.py
@@ -17,6 +17,9 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
